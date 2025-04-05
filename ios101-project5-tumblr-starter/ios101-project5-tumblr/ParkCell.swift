1cbbc6b718926b5df85a12c8b2be0e8d26ea247a
Tal-Assaf05 <tassa002@fiu.edu>
2025-04-05 02:59:44 -0400

    Add files via upload

---
+++ ios101-project5-tumblr-starter/ios101-project5-tumblr/ParkCell.swift
@@ -0,0 +1,23 @@
+//
+//  ParkCell.swift
+//  ios101-project5-tumblr
+//
+//  Created by Tal Assaf on 4/4/25.
+//
+
+import UIKit
+
+class ParkCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
